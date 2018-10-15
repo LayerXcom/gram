57c32a37b7583a1926ecf90576a9c66f10bd5e0e
mathetake <cz.rk.t0415y.g@gmail.com>
2018-10-15 13:08:00 +0900

    first implementation of tinyRAM struct

--- tinyram/tinyram.go
+++ tinyram/tinyram.go
@@ -1,6 +1,29 @@
 package tinyram
 
-type tinyRAM struct{}
+type tinyRAM struct {
+	WordSize      int64
+	NumRegister   int64
+	ConditionFlag bool
+
+	// pc ... Program Counter
+	Pc int64
+
+	// Register ... the length should be $NumRegister$
+	// and  each item be $WordSize$-bits
+	Register []int64
+
+	// Memory ... the length should be $2^WordSize$
+	// and each word should be $WordSize$-bits
+	Memory []int64
+
+	// PrimaryInput ... represents primary input of the computation
+	// each word should be $WordSize$-bits
+	PrimaryInput []int64
+
+	// AuxiliaryInput ... represents auxiliary input of the computation
+	// each word should be $WordSize$-bits
+	AuxiliaryInput []int64
+}
 
 type tinyRAMInstance struct {
 	ram  tinyRAM
