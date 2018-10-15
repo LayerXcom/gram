e1fc74fd3e8439e49b3a186512c4d73e440b721c
mathetake <cz.rk.t0415y.g@gmail.com>
2018-10-15 11:26:50 +0900

    add more TODOs

--- tinyram/program.go
+++ tinyram/program.go
@@ -1,3 +1,9 @@
 package tinyram
 
 type program []instructionToken
+
+// parse assembly file of a given path
+func parseRawAsm(path string) (program, error) {
+	// TODO: implement
+	return nil, nil
+}
