be6486aab08c10049770e08f311ffe5cd9424b3b
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-10-15 12:47:40 +0900

    Add constant instructions

--- tinyram/instruction.go
+++ tinyram/instruction.go
@@ -4,6 +4,36 @@ type instruction string
 
 // TODO: list all the instruction used by TinyRAM
 
+const (
+	AND    instruction = "AND"
+	OR     instruction = "OR"
+	XOR    instruction = "XOR"
+	NOT    instruction = "NOT"
+	ADD    instruction = "ADD"
+	SUB    instruction = "SUB"
+	MULL   instruction = "MULL"
+	UMULH  instruction = "UMULH"
+	SMULH  instruction = "SMULH"
+	UDIV   instruction = "UDIV"
+	UMOD   instruction = "UMOD"
+	SHL    instruction = "SHL"
+	SHR    instruction = "SHR"
+	CMPE   instruction = "SMPE"
+	CMPA   instruction = "CMPA"
+	CMPAE  instruction = "CMPAE"
+	CMPG   instruction = "CMPG"
+	CMPGE  instruction = "CMPGE"
+	MOV    instruction = "MOV"
+	CMOV   instruction = "CMOV"
+	JMP    instruction = "JMP"
+	CJMP   instruction = "CJMP"
+	CNJMP  instruction = "CNJMP"
+	STORE  instruction = "STORE"
+	LOAD   instruction = "LOAD"
+	READ   instruction = "READ"
+	ANSWER instruction = "ANSWER"
+)
+
 type instructionToken struct {
 	inst instruction
 	r1   int64
