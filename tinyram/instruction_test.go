bc705a3f8973f9a2680a4305ad8067815c715cb6
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-10-15 19:30:33 +0900

    impl testAddOperation

--- tinyram/instruction_test.go
+++ tinyram/instruction_test.go
@@ -1,1 +1,29 @@
-package tinyram_test
+package tinyram
+
+import (
+	"fmt"
+	"testing"
+	
+	"gotest.tools/assert"
+)
+
+func TestAddOperation(t *testing.T) {
+	cases := []struct {
+		tRAM *tinyRAM
+		r1, r2, r3 int64
+		expected int64
+	}{{
+		tRAM: &tinyRAM{10, 10, false, 0, [10], [2^10], [1], [1], program{instruction("AND"), 1, 2, 10}, false},
+		r1: 1,
+		r2: 2
+		r3: 10,
+		expected: 2 & 10,
+	}}
+
+	for n, tc := range cases {
+		tcc := tc
+		t.Run(fmt.Sprintf("$d-th unit test", n), func(t *testing.T) {
+			assert.Equal(t, tRAM.Register[r1], expected)
+		})
+	}
+}
