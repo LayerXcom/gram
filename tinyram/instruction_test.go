60431ad5ae5bf10a9956a569fee5780abd2c1059
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-10-15 23:48:42 +0900

    add andTest

--- tinyram/instruction_test.go
+++ tinyram/instruction_test.go
@@ -7,23 +7,33 @@ import (
 	"gotest.tools/assert"
 )
 
-func TestAddOperation(t *testing.T) {
+func TestAndOperation(t *testing.T) {
 	cases := []struct {
 		tRAM *tinyRAM
-		r1, r2, r3 int64
+		r1  int64
+		r2  int64
+		r3  int64
 		expected int64
-	}{{
-		tRAM: &tinyRAM{10, 10, false, 0, [10], [2^10], [1], [1], program{instruction("AND"), 1, 2, 10}, false},
-		r1: 1,
-		r2: 2
-		r3: 10,
-		expected: 2 & 10,
-	}}
+	}
+	{
+		{
+			tRAM: &tinyRAM{					
+				Register: []int64{0,0,3},
+				ConditionFlag: true,		
+			},
+			r1: 0,
+			r2: 2,
+			r3: 10,
+			expected: 3 & 10,
+		}
+	}
 
 	for n, tc := range cases {
 		tcc := tc
 		t.Run(fmt.Sprintf("$d-th unit test", n), func(t *testing.T) {
-			assert.Equal(t, tRAM.Register[r1], expected)
+			andOperation(*tcc.tRAM, tcc.r1, tcc.r2, tcc.r3)
+			assert.Equal(t, tcc.expected, *tcc.tRAM.Register[r1])
+			assert.Equal(t, false, tcc.tRAM.ConditionFlag)
 		})
 	}
 }
