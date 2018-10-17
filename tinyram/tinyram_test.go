7ded54ee0d00cbc0d2d85c4ae15e67e3134deade
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-10-17 16:22:56 +0900

    impl TestExecution

--- tinyram/tinyram_test.go
+++ tinyram/tinyram_test.go
@@ -1,1 +1,31 @@
 package tinyram
+
+import (
+	"fmt"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestExecution(t *testing.T) {
+	cases := []struct {
+		path string		
+	}{
+		{
+			path: "../example/testMemStore.asm",
+		},
+	}
+
+	for n, tc := range cases {
+		tcc := tc
+		t.Run(fmt.Sprintf("%d-th unit test", n), func(t *testing.T) {			
+			tRAM, err := GetTinyRAMInstance(tcc.path, 10, []uint64{2}, []uint64{2})
+			if err != nil {
+				t.Fatalf("get the tinyRAM instnce failed")
+			}
+
+			tRAM.Exec(9)
+			assert.Equal(t, true, tRAM.Accept)
+		})
+	}
+}
